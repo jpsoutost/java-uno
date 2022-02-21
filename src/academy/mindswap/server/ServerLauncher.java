844ba6db96bde3135e8cd7f23ddb8acff9649553
João Souto <jpsoutost@gmail.com>
2022-02-21 17:01:50 +0000

    initial commit, server-client example

---
+++ src/academy/mindswap/server/ServerLauncher.java
@@ -0,0 +1,15 @@
+package academy.mindswap.server;
+
+import java.io.IOException;
+
+public class ServerLauncher {
+
+    public static void main(String[] args) {
+        Server server = new Server();
+        try {
+            server.start(8082);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
