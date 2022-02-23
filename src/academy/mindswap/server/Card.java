4d5f5932c89ba84bc9c019c3f59281c9032037a9
Maria João Morais <mariajoaomorais@mindswap.academy>
2022-02-23 12:46:02 +0000

    Documentation Card,PlayerDeck and Command

--- src/academy/mindswap/server/Card.java
+++ src/academy/mindswap/server/Card.java
@@ -1,18 +1,32 @@
 package academy.mindswap.server;
 
+/**
+ * A class that represents the cards of the game.
+ */
 public class Card {
     private final CardColors color;
     private final int number;
 
+    /**
+     * Method that create a card.
+     * @param color Define the card color.
+     * @param number Define the card number.
+     */
     public Card(CardColors color, int number) {
         this.color = color;
         this.number = number;
     }
 
+    /**
+     * @return the color of the card, by a ENUM class.
+     */
     public CardColors getColor() {
         return color;
     }
 
+    /**
+     * @return The number of the card.
+     */
     public int getNumber() {
         return number;
     }
--- src/academy/mindswap/server/PlayerDeck.java
+++ src/academy/mindswap/server/PlayerDeck.java
@@ -6,19 +6,32 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/**
+ * A class that represents the deck of cards of a player.
+ */
 public class PlayerDeck {
     private List<Card> playerDeck;
     private Client client;
 
+    /**
+     * A player deck is initialized with an empty array list.
+     * @param client The player who will receive the deck.
+     */
     public PlayerDeck(Client client) {
         this.client = client;
         this.playerDeck = new ArrayList<>();
     }
 
+    /**
+     * @return A deck of cards to a player.
+     */
     public List<Card> getPlayerDeck() {
         return playerDeck;
     }
 
+    /**
+     * @return A client.
+     */
     public Client getClient() {
         return client;
     }
