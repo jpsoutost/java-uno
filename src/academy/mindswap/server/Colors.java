491908b788afe35f625fe463643b7a629f2815f5
João Souto <jpsoutost@gmail.com>
2022-02-27 08:38:23 +0000

    update quit command dynamics

--- src/academy/mindswap/server/Colors.java
+++
@@ -1,9 +0,0 @@
-package academy.mindswap.server;
-
-public class Colors {
-    public static final String BLUE = ConsoleColors.BLUE;
-    public static final String GREEN = ConsoleColors.GREEN;
-    public static final String RED = ConsoleColors.RED;
-    public static final String YELLOW = ConsoleColors.YELLOW;
-    public static final String PURPLE = ConsoleColors.PURPLE;
-}
